b20c947eaa96a163c800a58838f2f3d8ff60bd0a
Dahan Schuster <dan.plschuster@gmail.com>
2020-04-29 20:44:28 -0300

    Cria a rota de criação de repositórios
    Cria a rota POST /repositories, que recebe os dados do novo repositório
    pelo corpo da requisição e utiliza o celebrate para validar tais dados
    e o uuid para gerar um novo ID único para o objeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { celebrate, Segments, Joi } = require('celebrate')
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -17,8 +18,39 @@ app.get("/repositories", (request, response) => {
     return response.json(repositories)
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+/**
+ * Deve receber title, url e techs dentro do corpo da requisição, sendo a URL o 
+ * link para o github desse repositório. Ao cadastrar um novo projeto, ele deve 
+ * ser armazenado dentro de um objeto no seguinte formato: 
+ * 	{ 
+ * 		id: "uuid",
+ * 		title: 'Desafio Node.js',
+ * 		url: 'http://github.com/...',
+ * 		techs: ["Node.js", "..."],
+ * 		likes: 0
+ * 	}; 
+ * Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
+ */
+app.post("/repositories", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        url: Joi.string().required(),
+        techs: Joi.array()
+    })
+}), (request, response) => {
+    const { title, url, techs } = request.body
+
+    const repository = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0
+    }
+
+    repositories.push(repository)
+
+    return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
