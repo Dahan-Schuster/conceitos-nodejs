f4468d4cb4e65fee24ca91250d2936a21f53d885
Dahan Schuster <dan.plschuster@gmail.com>
2020-04-29 20:46:21 -0300

    Cria a rota de edição de repositórios
    Cria a rota PUT /repositories/:id, que recebe os ID do repositório a ser
    alterado pela rota e os novos dados pelo corpo da requisição
    A rota utiliza o método isUuid do pacote uuid para validar o ID enviado

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const { celebrate, Segments, Joi } = require('celebrate')
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -53,8 +53,49 @@ app.post("/repositories", celebrate({
     return response.json(repository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+/**
+ * Deve alterar apenas o title, a url e as techs do repositório que possua o id 
+ * igual ao id presente nos parâmetros da rota
+ */
+app.put("/repositories/:id", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string(),
+        url: Joi.string(),
+		techs: Joi.array(),
+		likes: Joi.invalid()
+    })
+}), (request, response) => {
+	const { title, url, techs } = request.body
+	const { id } = request.params
+
+	if (!isUuid(id)) {
+		return response.status(400).json({ error: 'ID inválido' })
+	}
+
+	const repositoryIndex = repositories
+		.findIndex(repository => repository.id === id)
+
+	if (repositoryIndex < 0) {
+		return response.status(400).json({
+			error: 'Nenhum repositório encontrado com o ID enviado'
+		})
+
+	}
+
+	const oldRepository = repositories[repositoryIndex]
+
+	const newRepository = {
+		id,
+		title: title ? title : oldRepository.title,
+		url: url ? url : oldRepository.url,
+		techs: techs ? techs : oldRepository.techs,
+		likes: oldRepository.likes
+	}
+
+	repositories[repositoryIndex] = newRepository
+
+	return response.json(newRepository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
